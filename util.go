b27ed0f4f018d98dfd069559c187c2b12311d8cb
Doridian <git@doridian.net>
2025-03-12 15:56:12 -0700

    autoport

---
+++ util.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/quic-go/quic-go"
+)
+
+func happyLoop(stream quic.Stream) error {
+	defer stream.Close()
+	quicStream = stream
+
+	sendReady()
+	log.Printf("Stream open!")
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
